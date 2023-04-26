d67facdc750864fe07f30a6d5b7cc6e36d9c519e
Anik <naimur.rezaa@gmail.com>
2023-04-26 13:34:05 +0600

    done

--- src/components/Navigation/LeftNav/Categories.jsx
+++ src/components/Navigation/LeftNav/Categories.jsx
@@ -10,15 +10,15 @@ const Categories = () => {
       .then((data) => setCategories(data));
   }, []);
   return (
-    <div className="bg-light p-3 rounded  box">
+    <div className="bg-light p-3 rounded ">
       {categories.map((ct, idx) => (
-        <p key={idx}>
+        <p key={idx} className=" gy-1 p-2 bg-white rounded">
           <NavLink
             to={`/categories/${ct.id}`}
             className={({ isActive, isLoading }) =>
               isActive
-                ? "text-decoration-none  text-muted fw-semibold bg-warning  rounded  d-block "
-                : "text-decoration-none    text-muted fw-semibold"
+                ? "text-decoration-none fw-semibold   p-0 m-0 text-warning  rounded  d-block "
+                : "text-decoration-none    text-muted  fw-semibold"
             }
           >
             {ct.name}
